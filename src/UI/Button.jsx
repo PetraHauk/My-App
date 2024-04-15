102be510803ab4114623e6aed243e928895ed76e
Petra Haukkala <petra.haukkala@gmail.com>
2024-04-15 15:27:47 +0300

    Media/single View + Tailwind

---
+++ src/UI/Button.jsx
@@ -0,0 +1,21 @@
+import Home from "../components/Home.jsx";
+import PropTypes from "prop-types";
+
+const Button = (props) => {
+  const {text, handleClick} = props;
+  return (
+    <>
+      <button className='m-3 mt-0 p-3 rounded-lg bg-stone-500 text-stone-100'
+        onClick={handleClick}>
+        {text}
+      </button>
+    </>
+  )
+};
+
+Button.propTypes = {
+  item: PropTypes.object,
+  handleClick: PropTypes.func.isRequired,
+};
+
+export default Button;
